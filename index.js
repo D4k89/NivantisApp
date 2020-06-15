2fac29367d4b36e4b9a16c78c293ea29ccc842d8
Koneko-Chan98 <61748376+Koneko-Chan98@users.noreply.github.com>
2020-06-15 21:03:25 +0200

    Add files via upload
    ouverture d'une session avec recherche de quelque chose dedans ( qui n'affiche rien pour le moment )

--- index.js
+++ index.js
@@ -7,5 +7,29 @@ import App from './App';
 // the environment is set up appropriately
 registerRootComponent(App);
 const sqlite3 = require("sqlite3").verbose();
-const sqlite3 = require("express").verbose();
-const sqlite3 = require("path").verbose();
+// ouerture de la base de donnees avec veriffication d erreur
+let db = new sqlite3.Database('.\NivantisDatabase.db');
+let db = new sqlite3.Database('.\NivantisDatabase.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the NivantisDatabase SQlite database.');
+});
+//recherhe dans la base de donnees
+let sql = 'SELECT * FROM Adresse';
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row.name);
+  });
+});
+
+// fermeture de la base de donnees avec veriffication d erreur
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
